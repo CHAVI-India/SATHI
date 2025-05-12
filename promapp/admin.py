4c615cbc158fd6e3670fa4dbde8071ba16064817
Santam Chakraborty <drsantam@gmail.com>
2025-05-12 09:16:48 +0530

    Refactor models and admin registration for scales. Introduced ConstructScale, LikertScale, and RangeScale models with corresponding response options. Updated Item model to handle response types and added validation logic. Enhanced admin interface for better management of scales and response options.

--- promapp/admin.py
+++ promapp/admin.py
@@ -2,7 +2,34 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Instrument)
-admin.site.register(Scale)
+
+class LikertScaleResponseOptionInline(admin.StackedInline):
+    model = LikertScaleResponseOption
+    extra = 1
+
+class RangeScaleResponseOptionInline(admin.StackedInline):
+    model = RangeScaleResponseOption
+    extra = 1
+
+
+@admin.register(LikertScale)
+class LikertScaleAdmin(admin.ModelAdmin):
+    inlines = [LikertScaleResponseOptionInline]
+    list_display = ('likert_scale_name',)
+    search_fields = ('likert_scale_name',)
+    list_filter = ('likert_scale_name',)
+    ordering = ('-created_date',)
+    readonly_fields = ('created_date', 'modified_date')
+
+@admin.register(RangeScale)
+class RangeScaleAdmin(admin.ModelAdmin):
+    inlines = [RangeScaleResponseOptionInline]
+    list_display = ('range_scale_name',)
+    search_fields = ('range_scale_name',)
+    list_filter = ('range_scale_name',)
+    ordering = ('-created_date',)
+    readonly_fields = ('created_date', 'modified_date')
+
+
+admin.site.register(ConstructScale)
 admin.site.register(Item)
-admin.site.register(ResponseOption)
--- promapp/models.py
+++ promapp/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.utils import timezone
 import uuid
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 
-class Instrument(models.Model):
+class ConstructScale(models.Model):
     '''
-    Instrument model.
+    Construct Scale model. Construct Scale refers to the collection of items that are used to measure a construct.
     '''
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255,null=True, blank=True)
@@ -15,87 +16,118 @@ class Instrument(models.Model):
 
     class Meta:
         ordering = ['-created_date']
-        verbose_name = 'Instrument'
-        verbose_name_plural = 'Instruments'
+        verbose_name = 'Construct Scale'
+        verbose_name_plural = 'Construct Scales'
 
     def __str__(self):
         return self.name
 
 
-class Scale(models.Model):
+class LikertScale(models.Model):
     '''
-    Scale model.
+    Likert scale type model. This is used to store the type of Likert Scale.
     '''
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    instrument = models.ForeignKey(Instrument, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255,null=True, blank=True)
+    likert_scale_name = models.CharField(max_length=255, null=True, blank=True, help_text = "The name of the Likert Scale")
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ['-created_date']
-        verbose_name = 'Scale'
-        verbose_name_plural = 'Scales'
+        verbose_name = 'Likert Scale Response'
+        verbose_name_plural = 'Likert Scale Responses'
 
     def __str__(self):
-        return self.name
+        return self.likert_scale_name
+
+class LikertScaleResponseOption(models.Model):
+    '''
+    Likert scale response options model. This is used to store the options for Likert Scale Responses.
+    '''
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    likert_scale = models.ForeignKey(LikertScale, on_delete=models.CASCADE)
+    option_text = models.CharField(max_length=255, null=True, blank=True, help_text = "The text to display for the option")
+    option_order = models.IntegerField(null=True, blank=True, help_text = "The order of the option. This will be a number.")
+    option_media = models.FileField(upload_to='likert_scale_response_options/', null=True, blank=True, help_text = "The media to display for the option. This will be an audio, video or image.")
+    option_value = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, help_text = "The value to store for the option. This will be a number with upto 2 decimal places.")
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['option_order']
+        verbose_name = 'Likert Scale Response Option'
+        verbose_name_plural = 'Likert Scale Response Options'
+        unique_together = ['option_text', 'option_order', 'option_value']
+
+    def __str__(self):
+        return self.option_text
+
+class RangeScale(models.Model):
+    '''
+    Range scale model. This is used to store the range of values for a range scale.
+    '''
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    range_scale_name = models.CharField(max_length=255, null=True, blank=True, help_text = "The name of the Range Scale")
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_date']
+        verbose_name = 'Range Scale'
+        verbose_name_plural = 'Range Scales'
+
+    def __str__(self):
+        return self.range_scale_name
+
+class RangeScaleResponseOption(models.Model):
+    '''
+    Range scale response options model. This is used to store the options for Range Scale Responses.
+    '''
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    range_scale = models.ForeignKey(RangeScale, on_delete=models.CASCADE)
+    min_value = models.DecimalField(max_digits=10, decimal_places=2, help_text = "The minimum value for the range scale")
+    min_value_text = models.CharField(max_length=255, null=True, blank=True, help_text = "The text to display for the minimum value")
+    max_value = models.DecimalField(max_digits=10, decimal_places=2, help_text = "The maximum value for the range scale")
+    max_value_text = models.CharField(max_length=255, null=True, blank=True, help_text = "The text to display for the maximum value")
+    increment = models.DecimalField(max_digits=10, default=1, decimal_places=2, null=True, blank=True, help_text = "The increment for the range scale. Must be more than 0")
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_date']
+        verbose_name = 'Range Scale Response Option'
+        verbose_name_plural = 'Range Scale Response Options'
+
+    def __str__(self):
+        return f"{self.range_scale.range_scale_name} - {self.min_value_text} to {self.max_value_text}"
+    
+    def validate_increment(self):
+        if self.min_value and self.max_value and self.increment:
+            if self.min_value > self.max_value:
+                raise ValueError("Minimum value cannot be greater than maximum value")
+            if self.increment <= 0:
+                raise ValueError("Increment must be greater than 0")
+            if (self.max_value - self.min_value) % self.increment != 0:
+                raise ValueError("Maximum value minus minimum value must be divisible by increment")
+
 
 class ResponseTypeChoices(models.TextChoices):
     TEXT = 'Text', 'Text Response'
     NUMBER = 'Number', 'Numeric Response'
-    BOOLEAN = 'Boolean', 'Yes/No Response'
     LIKERT = 'Likert', 'Likert Scale'
-    RANKING = 'Ranking', 'Ranking Response'
     RANGE = 'Range', 'Range Response'
 
-class ResponseOption(models.Model):
-    '''
-    ResponseOption model to store possible options for each response type.
-    '''
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    item = models.ForeignKey('Item', on_delete=models.CASCADE, related_name='response_options')
-    option_text = models.CharField(max_length=255)
-    option_value = models.CharField(max_length=255, null=True, blank=True)
-    order = models.IntegerField(default=0)
-    # Range specific fields
-    min_value = models.IntegerField(null=True, blank=True)  # For Range type
-    max_value = models.IntegerField(null=True, blank=True)  # For Range type
-    increment_value = models.IntegerField(null=True, blank=True)  # For Range type
-    created_date = models.DateTimeField(auto_now_add=True)
-    modified_date = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ['order', 'created_date']
-        verbose_name = 'Response Option'
-        verbose_name_plural = 'Response Options'
-
-    def __str__(self):
-        return f"{self.item.name} - {self.option_text}"
-
-    def clean(self):
-        """
-        Validate that range-specific fields are only set for range type responses
-        """
-        from django.core.exceptions import ValidationError
-        if self.item.response_type == ResponseTypeChoices.RANGE:
-            if self.min_value is None or self.max_value is None or self.increment_value is None:
-                raise ValidationError("Range type responses require min_value, max_value, and increment_value")
-            if self.min_value >= self.max_value:
-                raise ValidationError("min_value must be less than max_value")
-            if self.increment_value <= 0:
-                raise ValidationError("increment_value must be greater than 0")
-        else:
-            if self.min_value is not None or self.max_value is not None or self.increment_value is not None:
-                raise ValidationError("min_value, max_value, and increment_value are only for range type responses")
 
 class Item(models.Model):
     '''
-    Item model.
+    Item model. Ensure full_clean() is called before saving in views and forms
     '''
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    scale = models.ForeignKey(Scale, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255,null=True, blank=True)
-    response_type = models.CharField(max_length=255, choices=ResponseTypeChoices.choices)
+    construct_scale = models.ForeignKey(ConstructScale, on_delete=models.CASCADE, help_text = "Each item can belong to a construct scale which is designed to measure a construct or domain related to the Patient Reported Outcome.")
+    name = models.CharField(max_length=255,null=True, blank=True, help_text = "The name of the item which will be displayed to the patient")
+    response_type = models.CharField(max_length=255, choices=ResponseTypeChoices.choices, help_text = "The type of response for the item")
+    likert_response = models.ForeignKey(LikertScale, on_delete=models.CASCADE, null=True, blank=True)
+    range_response = models.ForeignKey(RangeScale, on_delete=models.CASCADE, null=True, blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
 
@@ -107,21 +139,23 @@ class Item(models.Model):
     def __str__(self):
         return self.name
 
-    def get_response_options(self):
-        """
-        Returns the response options for this item based on its response type.
-        """
-        if self.response_type == ResponseTypeChoices.TEXT:
-            return self.response_options.filter(option_text__isnull=False)
-        elif self.response_type == ResponseTypeChoices.NUMBER:
-            return self.response_options.filter(option_text__isnull=False)
-        elif self.response_type == ResponseTypeChoices.BOOLEAN:
-            return self.response_options.filter(option_text__in=['Yes', 'No'])
-        elif self.response_type == ResponseTypeChoices.LIKERT:
-            return self.response_options.filter(option_text__isnull=False).order_by('order')
-        elif self.response_type == ResponseTypeChoices.RANKING:
-            return self.response_options.filter(option_text__isnull=False).order_by('order')
+    def clean(self):
+        if self.response_type == ResponseTypeChoices.LIKERT:
+            if not self.likert_response:
+                raise ValidationError({'likert_response': 'Likert Scale must be selected when response type is Likert'})
+            if self.range_response:
+                raise ValidationError({'range_response': 'Range Scale should not be selected when response type is Likert'})
+        
         elif self.response_type == ResponseTypeChoices.RANGE:
-            return self.response_options.filter(option_text__isnull=False).order_by('order')
-        return self.response_options.none()
-    
+            if not self.range_response:
+                raise ValidationError({'range_response': 'Range Scale must be selected when response type is Range'})
+            if self.likert_response:
+                raise ValidationError({'likert_response': 'Likert Scale should not be selected when response type is Range'})
+        
+        elif self.response_type in [ResponseTypeChoices.TEXT, ResponseTypeChoices.NUMBER]:
+            if self.likert_response:
+                raise ValidationError({'likert_response': 'Likert Scale should not be selected for Text or Number response types'})
+            if self.range_response:
+                raise ValidationError({'range_response': 'Range Scale should not be selected for Text or Number response types'})
+
+
